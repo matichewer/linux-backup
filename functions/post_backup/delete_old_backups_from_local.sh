88fe35f8ef2fbd3239bc5ca99b5b85f2a09636d9
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-12 10:55:07 -0300

    Change log remove older local backups

--- functions/post_backup/delete_old_backups_from_local.sh
+++ functions/post_backup/delete_old_backups_from_local.sh
@@ -4,5 +4,5 @@ delete_old_backups_from_local() {
     log DEBUG "Call delete_old_backups_from_local $@"
     local backup_dir="$1"
     find "$backup_dir" -type f -name "backup_*.7z" -mtime +30 -exec rm {} +
-    log INFO "Backups older than 1 minute have been deleted."
+    log INFO "Backups older than 30 days have been deleted."
 }
