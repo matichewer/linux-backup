adcab9a81d2d8302367617d206b7a1de0f5aa112
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-25 22:38:05 -0300

    10 days in local

--- functions/post_backup/delete_old_backups_from_local.sh
+++ functions/post_backup/delete_old_backups_from_local.sh
@@ -3,6 +3,6 @@
 delete_old_backups_from_local() {
     log DEBUG "Call delete_old_backups_from_local $@"
     local backup_dir="$1"
-    find "$backup_dir" -type f -name "backup_*.7z" -mtime +30 -exec rm {} +
-    log INFO "Backups older than 30 days have been deleted."
+    find "$backup_dir" -type f -name "backup_*.7z" -mtime +10 -exec rm {} +
+    log INFO "Backups older than 10 days have been deleted."
 }
